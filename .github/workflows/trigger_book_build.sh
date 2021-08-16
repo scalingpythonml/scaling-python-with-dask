1fe867526ee5c06010b8fbbcb0e20f26bf014d5a
Holden Karau <holden@pigscanfly.ca>
2021-08-16 16:12:50 -0700

    Die when bad things happen. Wow that's depressing.

--- .github/workflows/trigger_book_build.sh
+++ .github/workflows/trigger_book_build.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
+set -ex
 
 curl -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token ${GH_PAT}" --request POST  --data '{"event_type": "update-event"}' https://api.github.com/repos/${PIPELINE_ORG_NAME}/${PIPELINE_REPO_NAME}/dispatches
 echo "triggered an update-event"
-
-
