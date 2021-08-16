f63304465ff10fe2fdbc1e87dbef2e55a646322a
Holden Karau <holden@pigscanfly.ca>
2021-08-16 16:16:02 -0700

    We don't need no subtrees any more

--- update_subtree.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-if [ ! -d examples ]; then
-  git subtree add  --prefix=examples git@github.com:scalingpythonml/scalingpythonml.git master
-fi
-git subtree pull  --prefix=examples git@github.com:scalingpythonml/scalingpythonml.git master
