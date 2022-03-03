ab1f796f541618ba2090419ca55b73474abf4ab5
nadams <nadams@oreilly.com>
2022-03-03 05:31:54 -0800

    Edited examples/dask/Dask-Ch2-Hello-Worlds.py with Atlas code editor

--- examples/dask/Dask-Ch2-Hello-Worlds.py
+++ examples/dask/Dask-Ch2-Hello-Worlds.py
@@ -140,7 +140,7 @@ dask.compute(word_count)
 # In[ ]:
 
 
-#tag::wc_dataframe
+#tag::wc_dataframe[]
 import dask.dataframe as dd
 
 @dask.delayed
@@ -154,7 +154,7 @@ initial_df = dd.from_delayed(delayed_dfs)
 wc_df = initial_df.text.str.split().explode().value_counts()
 
 dask.compute(wc_df)
-#end::wc_dataframe
+#end::wc_dataframe[]
 
 
 # In[ ]:
