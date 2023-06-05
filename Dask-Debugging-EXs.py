c70010eb9aae3334b5eab83343605ae63f001d65
mkimmins <ml.kimmins@gmail.com>
2023-06-05 15:55:00 -0700

    Created Dask-Debugging-EXs.py

---
+++ Dask-Debugging-EXs.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import dask
+import dask.bag as bag
+import dask.dataframe as dd
+from distributed.client import futures_of
+import pandas
+from io import StringIO
+import numpy as np
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+urls = ["https://gender-pay-gap.service.gov.uk/viewing/download-data/2021",
+        "https://www.pigscanfly.ca"]
+
+
+# In[ ]:
+
+
+#tag::handle[]
+# Handling some potentially bad data, this assume line-by-line 
+raw_chunks = bag.read_text(urls, files_per_partition = 1, linedelimiter="FAAAAAAAAAAAAAAAARTS")
+def maybe_load_data(data):
+    try:
+        # Put your processing code here
+        return (pandas.read_csv(StringIO(data)), None)
+    except Exception as e:
+        return (None, (e, data))
+data = raw_chunks.map(maybe_load_data)
+data.persist()
+bad_data = data.filter(lambda x: x[0] is None)
+good_data = data.filter(lambda x: x[1] is None)
+#end::handle[]
+
+
+# In[ ]:
+
+
+dask.compute(bad_data)
+
+
+# In[ ]:
+
+
+
+
